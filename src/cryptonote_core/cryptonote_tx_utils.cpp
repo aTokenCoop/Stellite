de176e478d156697d02a79c4d20ddde81b78c99c
Hayzam <hayzam@gmail.com>
2018-06-19 20:38:42 +0530

    RCT changes

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -124,7 +124,7 @@ namespace cryptonote
 
     CHECK_AND_ASSERT_MES(summary_amounts == block_reward, false, "Failed to construct miner tx, summary_amounts = " << summary_amounts << " not equal block_reward = " << block_reward);
 
-    if (hard_fork_version >= 5)
+    if (hard_fork_version >= 4)
       tx.version = 2;
     else
       tx.version = 1;
